2551422e02b9e58b30cb1550348426abf8e2ee08
CedricPalacio <cedric.palacio@gmail.com>
2023-03-28 12:04:36 +0200

    creation d'un premier composant

---
+++ src/component.js
@@ -0,0 +1,4 @@
+function PokemonCard () {
+    return <figure src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">Pokemon</figure>
+}
+export default PokemonCard;
