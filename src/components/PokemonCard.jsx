1785b2a20605fdf5ad39a9ece28146ef5f886f88
CedricPalacio <cedric.palacio@gmail.com>
2023-03-29 10:44:37 +0200

    modif avec le nom du pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -12,8 +12,8 @@ const pokemonList = [
 
 
 function PokemonCard () {
-  const pokemon = pokemonList[0]
-  return (pokemon.imgSrc ? <figure> <img src={pokemon.imgSrc}></img><figcaption>{pokemon.name}</figcaption></figure> : <p>???</p> )
+  const pokemon = pokemonList[1]
+  return (pokemon.imgSrc ? <figure> <img src={pokemon.imgSrc}></img><figcaption>{pokemon.name}</figcaption></figure> : <figure><p>???</p><figcaption>{pokemon.name}</figcaption></figure> )
 }  
 
 export default PokemonCard;
